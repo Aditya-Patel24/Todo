41352a5293e6ef75025742e02683a939a0af099d
Aditya-Patel24 <adityasp243@gmail.com>
2024-04-10 23:59:26 +0530

    Refactor todo management functions in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,34 @@ import { TodoForm, TodoItem } from './components';
 function App() {
 const [todos, setTodos] = useState([]);
 const addTodo = (todo) => {
-    setTodos([{ id: Date.now(), ...todo }, ...todos]);
-}
-const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-}
-const updateTodo = (id, todo) => {
-    setTodos(todos.map(todo => todo.id === id ? { ...todo, todo } : todo));
-}
-const toggleComplete = (id) => {
-    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
-}
-useEffect(()=>{
-    const storedTodos = localStorage.getItem('todos');
-    if (storedTodos) {
-        setTodos(JSON.parse(storedTodos));
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
+
+    
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    //console.log(id);
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
+  }
+useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
     }
-},[]);
+  }, [])
+
 useEffect(()=>{
     localStorage.setItem('todos', JSON.stringify(todos));
 }
