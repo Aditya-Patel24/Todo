bfc50d1f5390da28163ae9a919ce27ca0456c0fb
Aditya-Patel24 <adityasp243@gmail.com>
2024-04-10 21:27:17 +0530

    Update publicPath and base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-module.exports = {
-  publicPath: '/Todo/'
-}
-
 export default defineConfig({
   base: '/repo/',
   plugins: [react()]
