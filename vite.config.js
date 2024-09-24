146c4fb5c063c990a1e613a32332c0434e483451
Aditya-Patel24 <adityasp243@gmail.com>
2024-09-24 23:01:56 +0530

    Set base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
+  base: '/Todo/',  // Add this line to set the base URL
   plugins: [react()],
 })
